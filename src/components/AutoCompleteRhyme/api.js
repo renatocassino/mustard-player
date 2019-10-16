1e500e910b39064c7cd8e4571b4f93998623c0e8
Renato Cassino <renatocassino@gmail.com>
2019-10-15 21:50:04 -0300

    Change endpoint

--- src/components/AutoCompleteRhyme/api.js
+++ src/components/AutoCompleteRhyme/api.js
@@ -4,10 +4,10 @@ export const getWordsToRhyme = async (word) => {
   try {
     let baseUrl = 'http://localhost:8000'
     // if (typeof window !== 'undefined' && window.location.host.match(/github/i)) {
-      baseUrl = 'https://mustard-api.herokuapp.com'
+    baseUrl = 'https://mustard-api.herokuapp.com'
     // }
 
-    const response = await fetch(`${baseUrl}/api/v1/words/pt-br/${word}`)
+    const response = await fetch(`${baseUrl}/api/v1/rhymes/pt-br/${word}`)
     const { data } = await response.json()
 
     return data.words.slice(0, 30)
