d758db4fd88f6e689a8e071477dd9b96be117b8f
Renato Cassino <renatocassino@gmail.com>
2019-11-20 15:55:53 -0300

    Search with debounce

--- src/components/AutoCompleteRhyme/AutoCompleteRhyme.js
+++ src/components/AutoCompleteRhyme/AutoCompleteRhyme.js
@@ -1,24 +1,74 @@
 import React, { useState } from 'react'
 import ListOfWords from './ListOfWords'
+import { getWordsToRhyme } from '../../api/rhyme'
 import TextField from '@material-ui/core/TextField'
+import debounce from 'lodash/debounce'
 import Card from '@material-ui/core/Card'
+import { ApolloConsumer } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const RHYMES_WORDS = gql`
+query($word: String) {
+  getRhymes(word: $word) {
+    links {
+      last
+      previous
+      next
+    }
+    data {
+      language
+      words
+    }
+  }
+}
+`
+
+const debounceGet = debounce(({
+  currentWord, setListOfWords, setLoading, client,
+}) => {
+  if (currentWord === '') return
+
+  client.query({
+    query: RHYMES_WORDS,
+    variables: { word: currentWord }
+  }).then(({ data, loading }) => {
+    setLoading(loading)
+    setListOfWords(data.getRhymes.data.words || [])
+  });
+
+
+}, 500)
 
 const AutoCompleteRhyme = () => {
   const [word, setWord] = useState('')
+  const [listOfWords, setListOfWords] = useState([])
+  const [loading, setLoading] = useState(false)
 
   return (
-    <Card>
-      <TextField
-          id="standard-name"
-          label="Word to rhyme"
-          className={''}
-          value={word}
-          onChange={(e) => setWord(e.target.value)}
-          margin="normal"
-        />
+    <ApolloConsumer>
+      {client => (
+        <Card>
+          <TextField
+              id="standard-name"
+              label="Word to rhyme"
+              className={''}
+              value={word}
+              onChange={(e) => {
+                setWord(e.target.value)
+                debounceGet({
+                  currentWord: e.target.value,
+                  setListOfWords,
+                  client,
+                  setLoading,
+                })
+              }}
+              margin="normal"
+            />
 
-        <ListOfWords word={word} />
-    </Card>
+            <ListOfWords loading={loading} word={word} words={listOfWords} />
+        </Card>
+      )}
+    </ApolloConsumer>
   )
 }
 
--- src/components/AutoCompleteRhyme/ListOfWords.js
+++ src/components/AutoCompleteRhyme/ListOfWords.js
@@ -2,59 +2,32 @@ import React, { useEffect, useState } from 'react'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
-import debounce from 'lodash/debounce'
-import { getWordsToRhyme } from '../../api/rhyme'
-
-import { useQuery } from '@apollo/react-hooks';
-import { gql } from 'apollo-boost';
-
-const RHYMES_WORDS = gql`
-query($word: String) {
-  getRhymes(word: $word) {
-    links {
-      last
-      previous
-      next
-    }
-    data {
-      language
-      words
-    }
-  }
-}
-`
-
-const debounceGet = debounce((currentWord, setListOfWords) => {
-  getWordsToRhyme(currentWord).then(list => {
-    setListOfWords(list)
-  })
-}, 500)
 
 const ListOfWords = ({
-  word
+  loading,
+  word,
+  words = [],
 }) => {
-  let listOfWords = []
-  const { loading, error, data } = useQuery(RHYMES_WORDS, { variables: { word } })
-  if (loading) return <div>Loading...</div>
-
-  if (error) {
-    console.log(error)
-    listOfWords = []
-  } else {
-    listOfWords = data.getRhymes.data.words || []
-  }
+  if (loading) return <div>Loading....</div>
 
   return (
     <div>
       <List dense style={{ maxHeight: '300px', overflowY: 'auto' }}>
-        {listOfWords && listOfWords.length === 0 && (
+        {word === '' && (
+          <ListItem>
+            <ListItemText
+              primary={'Digite uma palavra para buscar rimas'}
+            />
+          </ListItem>
+        )}
+        {words && word !== '' && words.length === 0 && (
           <ListItem>
             <ListItemText
               primary={'Nenhuma palavra encontrada'}
             />
           </ListItem>
         )}
-        {listOfWords && listOfWords.map(currentWord => (
+        {words && word !== '' && words.map(currentWord => (
           <ListItem>
             <ListItemText
               primary={currentWord}
@@ -62,7 +35,6 @@ const ListOfWords = ({
           </ListItem>
         ))}
       </List>
-
     </div>
   )
 }
