ef4823a03269f354fdba2d573490597317305eec
Renato Cassino <renatocassino@gmail.com>
2019-11-20 17:41:03 -0300

    8c067a5

---
+++ precache-manifest.08e41964314461c673133fd1212492f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d3a544b54d735b11424c1f850fa5948",
+    "url": "http://mustardplayer.io/index.html"
+  },
+  {
+    "revision": "23dd97ad6ea3c0fedac9",
+    "url": "http://mustardplayer.io/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7f9314d5d91ae0a30aba",
+    "url": "http://mustardplayer.io/static/js/2.b7570a7c.chunk.js"
+  },
+  {
+    "revision": "23dd97ad6ea3c0fedac9",
+    "url": "http://mustardplayer.io/static/js/main.e308883b.chunk.js"
+  },
+  {
+    "revision": "2d90743e84b17f9b24c7",
+    "url": "http://mustardplayer.io/static/js/runtime-main.302da2a4.js"
+  }
+]);
